aa8ce681b69e8cbe47e39f1ffb7cce1aa702e802
Gnatienko <gnatienko@gmail.com>
2022-01-02 17:18:18 +0200

    react-create-app on Heroku9

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,7 @@
 import React from "react";
+//git add .
+//git commit -m "react-create-app on Heroku8"
+//git push heroku master
 
 const Results = ({ translations }) => {
   return translations.length ? ( //аналог ифа
@@ -7,11 +10,17 @@ const Results = ({ translations }) => {
       <div>
         {translations.map((translation, index) => (
           <p key={index}> 
-            {translation[0]}: {translation[1]}
+          <div class="translation">
+            <spun>
+              {translation[1]}
+            </spun>
+          </div>
+            {translation[0]}
           </p>
         ))}
       </div>
     </div>
+    // выше оборацивание в р
     // key должен быть при большом количестве одинаковых элементов
   ) : null; //продолжение?
 };
